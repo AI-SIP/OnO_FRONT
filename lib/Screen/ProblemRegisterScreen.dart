0b708ff201a3a5b9a6fb8763e15517ee2b7c52bd
KiSeungMin <ksm011212@gmail.com>
2024-09-07 15:41:10 +0900

    code refactoring : Problem Register Screen

--- lib/Screen/ProblemRegisterScreen.dart
+++ lib/Screen/ProblemRegisterScreen.dart
@@ -34,8 +34,7 @@ class ProblemRegisterScreenState extends State<ProblemRegisterScreen> {
 
     if (widget.problem != null) {
       _selectedDate = widget.problem!.solvedAt ?? DateTime.now();
-      _sourceController =
-          TextEditingController(text: widget.problem!.reference);
+      _sourceController = TextEditingController(text: widget.problem!.reference);
       _notesController = TextEditingController(text: widget.problem!.memo);
     } else {
       _selectedDate = DateTime.now();
@@ -63,6 +62,218 @@ class ProblemRegisterScreenState extends State<ProblemRegisterScreen> {
     });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeHandler>(context);
+
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                _buildDatePickerSection(themeProvider),
+                const SizedBox(height: 20),
+                buildSection(
+                  '출처',
+                  Icons.info,
+                  _buildStyledTextField(
+                    _sourceController,
+                    '문제집, 페이지, 문제번호 등 문제의 출처를 작성해주세요!',
+                    themeProvider,
+                  ),
+                ),
+                buildSection(
+                  '문제',
+                  Icons.camera_alt,
+                  buildImagePicker('problemImage', _problemImage, widget.problem?.problemImageUrl),
+                ),
+                buildSection(
+                  '해설',
+                  Icons.camera_alt,
+                  buildImagePicker('answerImage', _answerImage, widget.problem?.answerImageUrl),
+                ),
+                buildSection(
+                  '나의 풀이',
+                  Icons.camera_alt,
+                  buildImagePicker('solveImage', _solveImage, widget.problem?.solveImageUrl),
+                ),
+                buildSection(
+                  '한 줄 메모',
+                  Icons.edit,
+                  _buildStyledTextField(
+                    _notesController,
+                    '기록하고 싶은 내용을 간단하게 작성해주세요!',
+                    themeProvider,
+                    maxLines: 3,
+                  ),
+                ),
+                _buildActionButtons(themeProvider),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 문제 푼 날짜 선택 UI
+  Widget _buildDatePickerSection(ThemeHandler themeProvider) {
+    return Row(
+      children: <Widget>[
+        Icon(Icons.calendar_today, color: themeProvider.primaryColor),
+        const SizedBox(width: 10),
+        DecorateText(
+          text: '푼 날짜',
+          fontSize: 20,
+          color: themeProvider.primaryColor,
+        ),
+        const Spacer(),
+        TextButton(
+          onPressed: () => _service.showCustomDatePicker(
+            context,
+            _selectedDate,
+                (newDate) => setState(() {
+              _selectedDate = newDate;
+            }),
+          ),
+          child: DecorateText(
+            text: '${_selectedDate.year}년 ${_selectedDate.month}월 ${_selectedDate.day}일',
+            fontSize: 18,
+            color: themeProvider.primaryColor,
+          ),
+        ),
+      ],
+    );
+  }
+
+  // 이미지 선택 UI
+  Widget buildImagePicker(String imageType, XFile? image, String? existingImageUrl) {
+    final themeProvider = Provider.of<ThemeHandler>(context);
+    return Container(
+      height: 200,
+      decoration: BoxDecoration(
+        color: themeProvider.primaryColor.withOpacity(0.1),
+        border: Border.all(color: themeProvider.primaryColor, width: 2.0),
+      ),
+      child: Center(
+        child: image == null
+            ? existingImageUrl != null
+            ? GestureDetector(
+          onTap: () {
+            _service.showImagePicker(context, _onImagePicked, imageType);
+          },
+          child: DisplayImage(imagePath: existingImageUrl),
+        )
+            : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: Icon(Icons.image, color: themeProvider.primaryColor, size: 50),
+              onPressed: () {
+                _service.showImagePicker(context, _onImagePicked, imageType);
+              },
+            ),
+            DecorateText(
+              text: '아이콘을 눌러 이미지를 추가해주세요!',
+              color: themeProvider.primaryColor,
+              fontSize: 16,
+            ),
+          ],
+        )
+            : GestureDetector(
+          onTap: () {
+            _service.showImagePicker(context, _onImagePicked, imageType);
+          },
+          child: Image.file(File(image.path)),
+        ),
+      ),
+    );
+  }
+
+  // 텍스트 필드 생성 공통 함수
+  Widget _buildStyledTextField(
+      TextEditingController controller,
+      String hintText,
+      ThemeHandler themeProvider, {
+        int maxLines = 1,
+      }) {
+    return TextField(
+      controller: controller,
+      style: TextStyle(
+        fontFamily: 'font1',
+        color: themeProvider.primaryColor,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+      decoration: _buildInputDecoration(hintText, themeProvider),
+      maxLines: maxLines,
+    );
+  }
+
+  // InputDecoration 공통 설정 함수
+  InputDecoration _buildInputDecoration(String hintText, ThemeHandler themeProvider) {
+    return InputDecoration(
+      border: OutlineInputBorder(
+        borderSide: BorderSide(color: themeProvider.primaryColor, width: 2.0),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: themeProvider.primaryColor, width: 2.0),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: themeProvider.primaryColor, width: 2.0),
+      ),
+      fillColor: themeProvider.primaryColor.withOpacity(0.1),
+      filled: true,
+      hintText: hintText,
+      hintStyle: TextStyle(
+        fontFamily: 'font1',
+        color: themeProvider.primaryColor,
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  // 버튼 UI 생성 함수
+  Row _buildActionButtons(ThemeHandler themeProvider) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: _resetFields,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.white54,
+            foregroundColor: themeProvider.primaryColor,
+          ),
+          child: DecorateText(
+            text: widget.problem == null ? '등록 취소' : '수정 취소',
+            fontSize: 18,
+            color: themeProvider.primaryColor,
+          ),
+        ),
+        ElevatedButton(
+          onPressed: _submitProblem,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: themeProvider.primaryColor,
+            foregroundColor: Colors.white,
+          ),
+          child: DecorateText(
+            text: widget.problem == null ? '등록 완료' : '수정 완료',
+            fontSize: 18,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+
+  // 공통 UI 빌더 함수
   Widget buildSection(String title, IconData icon, Widget content) {
     final themeProvider = Provider.of<ThemeHandler>(context);
     return Column(
@@ -86,63 +297,12 @@ class ProblemRegisterScreenState extends State<ProblemRegisterScreen> {
     );
   }
 
-  Widget buildImagePicker(
-      String imageType, XFile? image, String? existingImageUrl) {
-    final themeProvider = Provider.of<ThemeHandler>(context);
-    return Container(
-      height: 200,
-      decoration: BoxDecoration(
-        color: themeProvider.primaryColor.withOpacity(0.1),
-        border: Border.all(
-          color: themeProvider.primaryColor,
-          width: 2.0,
-        ),
-      ),
-      child: Center(
-        child: image == null
-            ? existingImageUrl != null
-                ? GestureDetector(
-                    onTap: () {
-                      _service.showImagePicker(
-                          context, _onImagePicked, imageType);
-                    },
-                    child: DisplayImage(imagePath: existingImageUrl),
-                  )
-                : Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.image,
-                            color: themeProvider.primaryColor, size: 50),
-                        onPressed: () {
-                          _service.showImagePicker(
-                              context, _onImagePicked, imageType);
-                        },
-                      ),
-                      DecorateText(
-                        text: '아이콘을 눌러 이미지를 추가해주세요!',
-                        color: themeProvider.primaryColor,
-                        fontSize: 16,
-                      )
-                    ],
-                  )
-            : GestureDetector(
-                onTap: () {
-                  _service.showImagePicker(context, _onImagePicked, imageType);
-                },
-                child: Image.file(File(image.path)),
-              ),
-      ),
-    );
-  }
-
-  void _onImagePicked(XFile? pickedFile,
-      List<Map<String, int>?>? selectedColors, String imageType) {
+  // 이미지 선택 핸들러
+  void _onImagePicked(XFile? pickedFile, List<Map<String, int>?>? selectedColors, String imageType) {
     setState(() {
       if (imageType == 'problemImage') {
         _problemImage = pickedFile;
         _selectedColors = selectedColors;
-        print('selectedColors : ${selectedColors}');
       } else if (imageType == 'answerImage') {
         _answerImage = pickedFile;
       } else if (imageType == 'solveImage') {
@@ -151,6 +311,7 @@ class ProblemRegisterScreenState extends State<ProblemRegisterScreen> {
     });
   }
 
+  // 문제 등록 및 수정 제출
   void _submitProblem() {
     if (widget.problem == null) {
       final problemData = ProblemRegisterModel(
@@ -176,8 +337,7 @@ class ProblemRegisterScreenState extends State<ProblemRegisterScreen> {
         memo: _notesController.text == widget.problem!.memo
             ? null
             : _notesController.text,
-        solvedAt:
-            _selectedDate == widget.problem!.solvedAt ? null : _selectedDate,
+        solvedAt: _selectedDate == widget.problem!.solvedAt ? null : _selectedDate,
         problemImage: _problemImage,
         answerImage: _answerImage,
         solveImage: _solveImage,
@@ -185,196 +345,8 @@ class ProblemRegisterScreenState extends State<ProblemRegisterScreen> {
       );
 
       _service.updateProblem(context, updatedProblem, () {
-        Navigator.of(context).pop(true); // Return to the previous screen
+        Navigator.of(context).pop(true); // Return to the previous screen after editing
       });
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeHandler>(context);
-
-    return Scaffold(
-      body: GestureDetector(
-        onTap: () {
-          FocusScope.of(context).unfocus();
-        },
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                // 문제 푼 날짜 선택
-                Row(
-                  children: <Widget>[
-                    Icon(Icons.calendar_today,
-                        color: themeProvider.primaryColor),
-                    const SizedBox(width: 10),
-                    DecorateText(
-                      text: '푼 날짜',
-                      fontSize: 20,
-                      color: themeProvider.primaryColor,
-                    ),
-                    const Spacer(),
-                    TextButton(
-                      onPressed: () => _service.showCustomDatePicker(
-                        context,
-                        _selectedDate,
-                        (newDate) => setState(() {
-                          _selectedDate = newDate;
-                        }),
-                      ),
-                      child: DecorateText(
-                        text:
-                            '${_selectedDate.year}년 ${_selectedDate.month}월 ${_selectedDate.day}일',
-                        fontSize: 18,
-                        color: themeProvider.primaryColor,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                // 출처 입력
-                buildSection(
-                  '출처',
-                  Icons.info,
-                  TextField(
-                    controller: _sourceController,
-                    style: TextStyle(
-                      fontFamily: 'font1',
-                      color: themeProvider.primaryColor,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: themeProvider.primaryColor,
-                          width: 2.0,
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: themeProvider.primaryColor,
-                          width: 2.0,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: themeProvider.primaryColor,
-                          width: 2.0,
-                        ),
-                      ),
-                      fillColor: themeProvider.primaryColor.withOpacity(0.1),
-                      filled: true,
-                      hintText: '문제집, 페이지, 문제번호 등 문제의 출처를 작성해주세요!',
-                      hintStyle: TextStyle(
-                        fontFamily: 'font1',
-                        color: themeProvider.primaryColor,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                buildSection(
-                  '문제',
-                  Icons.camera_alt,
-                  buildImagePicker('problemImage', _problemImage,
-                      widget.problem?.problemImageUrl),
-                ),
-                buildSection(
-                  '해설',
-                  Icons.camera_alt,
-                  buildImagePicker('answerImage', _answerImage,
-                      widget.problem?.answerImageUrl),
-                ),
-                buildSection(
-                  '나의 풀이',
-                  Icons.camera_alt,
-                  buildImagePicker(
-                      'solveImage', _solveImage, widget.problem?.solveImageUrl),
-                ),
-                buildSection(
-                  '한 줄 메모',
-                  Icons.edit,
-                  TextField(
-                    controller: _notesController,
-                    style: TextStyle(
-                      fontFamily: 'font1',
-                      color: themeProvider.primaryColor,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textInputAction: TextInputAction.done,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: themeProvider.primaryColor,
-                          width: 2.0,
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: themeProvider.primaryColor,
-                          width: 2.0,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: themeProvider.primaryColor,
-                          width: 2.0,
-                        ),
-                      ),
-                      fillColor: themeProvider.primaryColor.withOpacity(0.1),
-                      filled: true,
-                      hintText: '기록하고 싶은 내용을 간단하게 작성해주세요!',
-                      hintStyle: TextStyle(
-                        fontFamily: 'font1',
-                        color: themeProvider.primaryColor,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    maxLines: 3,
-                  ),
-                ),
-                // 등록 취소 및 완료 버튼
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    ElevatedButton(
-                      onPressed: _resetFields,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white54,
-                        foregroundColor: themeProvider.primaryColor,
-                      ),
-                      child: DecorateText(
-                        text: widget.problem == null ? '등록 취소' : '수정 취소',
-                        fontSize: 18,
-                        color: themeProvider.primaryColor,
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: _submitProblem,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: themeProvider.primaryColor,
-                        foregroundColor: Colors.white,
-                      ),
-                      child: DecorateText(
-                        text: widget.problem == null ? '등록 완료' : '수정 완료',
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
