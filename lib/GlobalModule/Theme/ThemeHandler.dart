b6e75a7b16907387e8e5b8f38018e16ff13165ad
KiSeungMin <ksm011212@gmail.com>
2024-09-13 20:32:00 +0900

    feat : update themehandler => add lightenColor and darkenColor

--- lib/GlobalModule/Theme/ThemeHandler.dart
+++ lib/GlobalModule/Theme/ThemeHandler.dart
@@ -3,42 +3,33 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ThemeHandler with ChangeNotifier {
   // 기본 색상
-  Color _primaryColor = Colors.green;
-  Color _secondColor = Colors.lightGreen;
-  Color _thirdColor = Colors.teal;
+  Color _primaryColor = Colors.lightGreen;
+  Color _darkPrimaryColor = Colors.lightGreen;
+  Color _lightPrimaryColor = Colors.lightGreen;
+  //Color _secondColor = Colors.lightGreen;
+  //Color _thirdColor = Colors.teal;
 
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
   // 각 색상에 대한 getter
   Color get primaryColor => _primaryColor;
-  Color get secondColor => _secondColor;
-  Color get thirdColor => _thirdColor;
 
   // 색상을 변경하고 저장하는 메서드
   void changePrimaryColor(Color primaryColor) {
     _primaryColor = primaryColor;
+    _lightPrimaryColor = _lightenColor(primaryColor, 0.2);
+    _darkPrimaryColor = _darkenColor(primaryColor, 0.2);
     _saveColor('primaryColor', primaryColor);
-    notifyListeners();
-  }
-
-  // SecondColor와 ThirdColor도 저장할 수 있도록 수정
-  void changeSecondColor(Color secondColor) {
-    _secondColor = secondColor;
-    _saveColor('secondColor', secondColor);
-    notifyListeners();
-  }
-
-  void changeThirdColor(Color thirdColor) {
-    _thirdColor = thirdColor;
-    _saveColor('thirdColor', thirdColor);
+    _saveColor('lightPrimaryColor', _lightPrimaryColor);
+    _saveColor('darkPrimaryColor', _darkPrimaryColor);
     notifyListeners();
   }
 
   // 저장된 색상을 로드하는 메서드
   Future<void> loadColors() async {
-    _primaryColor = await _loadColor('primaryColor', Colors.green);
-    _secondColor = await _loadColor('secondColor', Colors.lightGreen);
-    _thirdColor = await _loadColor('thirdColor', Colors.teal);
+    _primaryColor = await _loadColor('primaryColor', Colors.lightGreen);
+    _lightPrimaryColor = await _loadColor('lightPrimaryColor', _lightenColor(Colors.lightGreen, 0.2));
+    _darkPrimaryColor = await _loadColor('darkPrimaryColor', _darkenColor(Colors.lightGreen, 0.2));
     notifyListeners();
   }
 
@@ -62,6 +53,16 @@ class ThemeHandler with ChangeNotifier {
     final hslColor = HSLColor.fromColor(color);
     final lightenedHslColor = hslColor.withLightness(
       (hslColor.lightness + amount).clamp(0.0, 1.0),
+    );
+    return lightenedHslColor.toColor();
+  }
+
+  // 더 밝은 색상을 생성하는 메서드
+  Color _darkenColor(Color color, double amount) {
+    assert(amount >= 0 && amount <= 1);
+    final hslColor = HSLColor.fromColor(color);
+    final lightenedHslColor = hslColor.withLightness(
+      (hslColor.lightness - amount).clamp(0.0, 1.0),
     );
     return lightenedHslColor.toColor();
   }
