393972a89769b10b1fa066f39840c3a30643b521
KiSeungMin <ksm011212@gmail.com>
2024-09-28 16:11:46 +0900

    Latex 형식의 텍스트 변환 기능 성공

--- lib/Model/TemplateType.dart
+++ lib/Model/TemplateType.dart
@@ -21,9 +21,9 @@ extension TemplateTypeExtension on TemplateType {
       case TemplateType.simple:
         return ['추천 과목 : 국어, 영어', '빠르게 오답노트를 등록하고, 편리한 복습을 하고 싶은 분들을 위한 템플릿입니다.'];
       case TemplateType.clean:
-        return ['추천 과목 : 사회 탐구', '필기 제거 기능을 통해 효율적인 복습을 하고 싶은 분들을 위한 템플릿입니다.'];
+        return ['추천 과목 : 사회 탐구, 과학 탐구', '필기 제거 기능을 통해 효율적인 복습을 하고 싶은 분들을 위한 템플릿입니다.'];
       case TemplateType.special:
-        return ['추천 과목 : 수학, 과학', '필기 제거 기능과 AI 문제 분석 기능을 통해 고도화된 복습을 하고 싶은 분들을 위한 템플릿입니다'];
+        return ['추천 과목 : 수학', '필기 제거 기능과 AI 문제 분석 기능을 통해 고도화된 복습을 하고 싶은 분들을 위한 템플릿입니다'];
     }
   }
 }
--- lib/Screen/ProblemRegister/Template/SpecialTemplate.dart
+++ lib/Screen/ProblemRegister/Template/SpecialTemplate.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tex/flutter_tex.dart';
 import 'package:provider/provider.dart';
+import '../../../GlobalModule/Util/LatexTextHandler.dart';
 import '../../../Provider/FoldersProvider.dart';
 
 class SpecialTemplate extends StatefulWidget {
@@ -7,7 +9,11 @@ class SpecialTemplate extends StatefulWidget {
   final String problemImageUrl;
   final List<Map<String, int>?>? colors; // 추가된 부분
 
-  const SpecialTemplate({required this.problemId, required this.problemImageUrl, required this.colors, Key? key})
+  const SpecialTemplate(
+      {required this.problemId,
+      required this.problemImageUrl,
+      required this.colors,
+      Key? key})
       : super(key: key);
 
   @override
@@ -29,7 +35,8 @@ class _SpecialTemplateState extends State<SpecialTemplate> {
     final provider = Provider.of<FoldersProvider>(context, listen: false);
 
     // processImageUrl 먼저 fetch
-    processImageUrl = await provider.fetchProcessImageUrl(widget.problemImageUrl, widget.colors);
+    processImageUrl = await provider.fetchProcessImageUrl(
+        widget.problemImageUrl, widget.colors);
 
     // analysisResult fetch
     analysisResult = await provider.fetchAnalysisResult(widget.problemImageUrl);
@@ -42,20 +49,50 @@ class _SpecialTemplateState extends State<SpecialTemplate> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Special Template 등록')),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.network(processImageUrl!),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text('Analysis Result: $analysisResult'),
-            ),
-          ],
+      appBar: AppBar(
+        title: const Text('Special Template 등록'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () async {
+            bool isDeleted =
+                await Provider.of<FoldersProvider>(context, listen: false)
+                    .deleteProblem(widget.problemId);
+            if (isDeleted) {
+              Navigator.pop(context);
+            }
+          },
         ),
       ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  Image.network(processImageUrl!),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: analysisResult != null
+                        ? TeXView(
+                            fonts: const [
+                              TeXViewFont(
+                                  fontFamily: 'HandWrite',
+                                  src: 'assets/fonts/HandWrite.ttf'),
+                            ],
+                            renderingEngine:
+                                const TeXViewRenderingEngine.mathjax(),
+                            child:
+                                LatexTextHandler.renderLatex(analysisResult!),
+                            style: const TeXViewStyle(
+                              elevation: 5,
+                              borderRadius: TeXViewBorderRadius.all(10),
+                              backgroundColor: Colors.white,
+                            ),
+                          )
+                        : const Text('No analysis result available'),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
