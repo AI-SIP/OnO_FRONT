1e6bbae057feb613fc15e8d174091df2ac2653c4
KiSeungMin <ksm011212@gmail.com>
2024-09-28 13:32:33 +0900

    TemplateType 구조체를 사용하도록 수정

---
+++ lib/Model/TemplateType.dart
@@ -0,0 +1,29 @@
+enum TemplateType {
+  simple,
+  clean,
+  special,
+}
+
+extension TemplateTypeExtension on TemplateType {
+  String get displayName {
+    switch (this) {
+      case TemplateType.simple:
+        return '심플 템플릿';
+      case TemplateType.clean:
+        return '클린 템플릿';
+      case TemplateType.special:
+        return '스페셜 템플릿';
+    }
+  }
+
+  List<String> get description {
+    switch (this) {
+      case TemplateType.simple:
+        return ['추천 과목 : 국어, 영어', '빠르게 오답노트를 등록하고, 편리한 복습을 하고 싶은 분들을 위한 템플릿입니다.'];
+      case TemplateType.clean:
+        return ['추천 과목 : 사회 탐구', '필기 제거 기능을 통해 효율적인 복습을 하고 싶은 분들을 위한 템플릿입니다.'];
+      case TemplateType.special:
+        return ['추천 과목 : 수학, 과학', '필기 제거 기능과 AI 문제 분석 기능을 통해 고도화된 복습을 하고 싶은 분들을 위한 템플릿입니다'];
+    }
+  }
+}
--- lib/Screen/TemplateSelectionScreen.dart
+++ lib/Screen/TemplateSelectionScreen.dart
@@ -1,9 +1,9 @@
-// TemplateSelectionScreen.dart
 import 'package:flutter/material.dart';
-import 'package:ono/GlobalModule/Theme/StandardText.dart';
+import 'package:ono/GlobalModule/Theme/HandWriteText.dart';
 import 'package:provider/provider.dart';
-import '../GlobalModule/Theme/HandWriteText.dart';
+import '../GlobalModule/Theme/StandardText.dart';
 import '../GlobalModule/Theme/ThemeHandler.dart';
+import '../Model/TemplateType.dart';
 
 class TemplateSelectionScreen extends StatelessWidget {
   const TemplateSelectionScreen({super.key});
@@ -25,38 +25,19 @@ class TemplateSelectionScreen extends StatelessWidget {
           Expanded(
             child: ListView(
               padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              children: [
-                const SizedBox(height: 10),
-                _buildTemplateItem(
-                  context: context,
-                  title: '심플 템플릿',
-                  descriptions: [
-                    '추천 과목 : 국어, 영어',
-                    '빠르게 오답노트를 등록하고, 편리한 복습을 하고 싶은 분들을 위한 템플릿입니다.',
-                  ], // description을 여러 필드로 분리
-                  themeProvider: themeProvider,
-                ),
-                const Divider(),
-                _buildTemplateItem(
-                  context: context,
-                  title: '클린 템플릿',
-                  descriptions: [
-                    '추천 과목 : 사회 탐구',
-                    '필기 제거 기능을 통해 효율적인 복습을 하고 싶은 분들을 위한 템플릿입니다.',
-                  ], // description을 여러 필드로 분리
-                  themeProvider: themeProvider,
-                ),
-                const Divider(),
-                _buildTemplateItem(
-                  context: context,
-                  title: '스페셜 템플릿',
-                  descriptions: [
-                    '추천 과목 : 수학, 과학',
-                    '필기 제거 기능과 AI 문제 분석 기능을 통해 고도화된 복습을 하고 싶은 분들을 위한 템플릿입니다',
-                  ], // description을 여러 필드로 분리
-                  themeProvider: themeProvider,
-                ),
-              ],
+              children: TemplateType.values.map((templateType) {
+                return Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    _buildTemplateItem(
+                      context: context,
+                      templateType: templateType,
+                      themeProvider: themeProvider,
+                    ),
+                    const Divider(),
+                  ],
+                );
+              }).toList(),
             ),
           ),
         ],
@@ -66,34 +47,31 @@ class TemplateSelectionScreen extends StatelessWidget {
 
   Widget _buildTemplateItem({
     required BuildContext context,
-    required String title,
-    required List<String> descriptions, // 여러 필드로 분리된 description 리스트
+    required TemplateType templateType,
     required ThemeHandler themeProvider,
   }) {
     return ListTile(
       contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
       title: HandWriteText(
-        text: title,
-        fontSize: 20,
+        text: templateType.displayName,
+        fontSize: 24,
         color: themeProvider.darkPrimaryColor,
       ),
       subtitle: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: descriptions.map((desc) {
+        children: templateType.description.map((desc) {
           return Padding(
-            padding: const EdgeInsets.only(top: 4.0), // 각 필드 사이에 여백 추가
+            padding: const EdgeInsets.only(top: 4.0),
             child: StandardText(
               text: desc,
-              fontSize: 14,
+              fontSize: 16,
               color: themeProvider.primaryColor.withOpacity(0.6),
             ),
           );
         }).toList(),
       ),
       onTap: () {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('$title 선택됨')),
-        );
+        Navigator.pushNamed(context, '/problemRegister', arguments: templateType);
       },
     );
   }
