16c305427fe222d116274db836d720af7908ac47
KiSeungMin <ksm011212@gmail.com>
2024-10-01 15:27:18 +0900

    feat : Special Template 가로 모드 버그 해결

--- lib/Screen/ProblemDetail/Template/SpecialProblemDetailTemplate.dart
+++ lib/Screen/ProblemDetail/Template/SpecialProblemDetailTemplate.dart
@@ -6,29 +6,28 @@ import '../../../GlobalModule/Theme/ThemeHandler.dart';
 import '../../../GlobalModule/Util/LatexTextHandler.dart';
 import '../../../Model/ProblemModel.dart';
 import '../ProblemDetailScreenWidget.dart';
-
-
 class SpecialProblemDetailTemplate extends StatelessWidget {
   final ProblemModel problemModel;
+  final ProblemDetailScreenWidget problemDetailScreenWidget = ProblemDetailScreenWidget();
 
-  const SpecialProblemDetailTemplate({required this.problemModel, Key? key}) : super(key: key);
+  SpecialProblemDetailTemplate({required this.problemModel, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeHandler>(context);
     final screenWidth = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: Stack(
-        children: [
-          ProblemDetailScreenWidget().buildBackground(themeProvider),
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 35.0),
+    return Stack(
+      children: [
+        problemDetailScreenWidget.buildBackground(themeProvider),
+        SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 35.0),
+            child: Container(
+              constraints: BoxConstraints(maxWidth: screenWidth), // 화면의 너비를 명시적으로 지정하여 무한 너비 방지
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  // 화면 너비에 따른 배치 결정
                   if (screenWidth > 600)
                     _buildWideLayout(context, themeProvider)
                   else
@@ -37,47 +36,53 @@ class SpecialProblemDetailTemplate extends StatelessWidget {
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
   /// 화면 너비가 600 이상일 때의 레이아웃
   Widget _buildWideLayout(BuildContext context, ThemeHandler themeProvider) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        // 좌측 영역 (푼 날짜와 문제 출처)
-        Flexible(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 30.0),
-              ProblemDetailScreenWidget().buildSolvedDate(problemModel.solvedAt, themeProvider),
-              const SizedBox(height: 30.0),
-              ProblemDetailScreenWidget().buildProblemReference(problemModel.reference, themeProvider),
-            ],
-          ),
-        ),
-        const SizedBox(width: 30.0), // 좌우 간격을 위한 여백
-        // 우측 영역 (문제 이미지)
-        Flexible(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 25.0),
-              ProblemDetailScreenWidget().buildImageSection(
-                context,
-                problemModel.processImageUrl,
-                '문제 이미지',
-                themeProvider.primaryColor,
-                themeProvider,
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // 좌측 영역 (푼 날짜와 문제 출처)
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 30.0),
+                  problemDetailScreenWidget.buildSolvedDate(problemModel.solvedAt, themeProvider),
+                  const SizedBox(height: 30.0),
+                  problemDetailScreenWidget.buildProblemReference(problemModel.reference, themeProvider),
+                ],
               ),
-              const SizedBox(height: 30.0),
-            ],
-          ),
+            ),
+            const SizedBox(width: 30.0), // 좌우 간격을 위한 여백
+            // 우측 영역 (문제 이미지)
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 25.0),
+                  problemDetailScreenWidget.buildImageSection(
+                    context,
+                    problemModel.processImageUrl,
+                    '문제 이미지',
+                    themeProvider.primaryColor,
+                    themeProvider,
+                  ),
+                  const SizedBox(height: 30.0),
+                ],
+              ),
+            ),
+          ],
         ),
-        _buildAnalysisExpansionTile(themeProvider, context),
+        const SizedBox(height: 20.0), // 위 아래 간격을 위한 여백
+        _buildAnalysisExpansionTile(themeProvider, context), // 정답 및 해설 확인을 중앙에 배치
       ],
     );
   }
@@ -88,11 +93,11 @@ class SpecialProblemDetailTemplate extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const SizedBox(height: 16.0),
-        ProblemDetailScreenWidget().buildSolvedDate(problemModel.solvedAt, themeProvider),
+        problemDetailScreenWidget.buildSolvedDate(problemModel.solvedAt, themeProvider),
         const SizedBox(height: 25.0),
-        ProblemDetailScreenWidget().buildProblemReference(problemModel.reference, themeProvider),
+        problemDetailScreenWidget.buildProblemReference(problemModel.reference, themeProvider),
         const SizedBox(height: 30.0),
-        ProblemDetailScreenWidget().buildImageSection(
+        problemDetailScreenWidget.buildImageSection(
           context,
           problemModel.processImageUrl,
           '문제 이미지',
@@ -105,19 +110,17 @@ class SpecialProblemDetailTemplate extends StatelessWidget {
     );
   }
 
-
-
   /// 분석 결과를 포함한 ExpansionTile 구성
   Widget _buildAnalysisExpansionTile(ThemeHandler themeProvider, BuildContext context) {
     return ExpansionTile(
       title: ProblemDetailScreenWidget.buildCenteredTitle('정답 및 해설 확인', themeProvider.primaryColor),
       children: [
         const SizedBox(height: 10.0),
-        ProblemDetailScreenWidget().buildSectionWithMemo(problemModel.memo, themeProvider),
+        problemDetailScreenWidget.buildSectionWithMemo(problemModel.memo, themeProvider),
         const SizedBox(height: 20.0),
-        ProblemDetailScreenWidget().buildLatexView(context, problemModel.analysis, themeProvider),
+        problemDetailScreenWidget.buildLatexView(context, problemModel.analysis, themeProvider),
         const SizedBox(height: 20.0),
-        ProblemDetailScreenWidget().buildImageSection(
+        problemDetailScreenWidget.buildImageSection(
           context,
           problemModel.problemImageUrl,
           '문제 원본 이미지',
@@ -125,7 +128,7 @@ class SpecialProblemDetailTemplate extends StatelessWidget {
           themeProvider,
         ),
         const SizedBox(height: 20.0),
-        ProblemDetailScreenWidget().buildImageSection(
+        problemDetailScreenWidget.buildImageSection(
           context,
           problemModel.answerImageUrl,
           '해설 이미지',
@@ -133,7 +136,7 @@ class SpecialProblemDetailTemplate extends StatelessWidget {
           themeProvider,
         ),
         const SizedBox(height: 20.0),
-        ProblemDetailScreenWidget().buildImageSection(
+        problemDetailScreenWidget.buildImageSection(
           context,
           problemModel.solveImageUrl,
           '풀이 이미지',
