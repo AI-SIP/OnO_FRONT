cdd9afaa21606ce4dc7c971810313bc0f42b3a96
KiSeungMin <ksm011212@gmail.com>
2024-10-30 22:51:07 +0900

    code refactoring: 코드 함수 구조로 변경

--- lib/Screen/ProblemPractice/PracticeNavigationButtons.dart
+++ lib/Screen/ProblemPractice/PracticeNavigationButtons.dart
@@ -30,61 +30,84 @@ class _PracticeNavigationButtonsState extends State<PracticeNavigationButtons> {
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeHandler>(context);
-    final problemIds = widget.practiceProvider.problemIds;
-    double screenHeight = MediaQuery.of(context).size.height;
-
-    if (problemIds.isEmpty) {
-      return const Center();
-    }
-
-    int currentIndex = problemIds.indexOf(widget.currentId);
-    int previousProblemId =
-    currentIndex > 0 ? problemIds[currentIndex - 1] : problemIds.first;
-    int nextProblemId =
-    currentIndex < problemIds.length - 1 ? problemIds[currentIndex + 1] : -1;
+    final screenHeight = MediaQuery.of(context).size.height;
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        TextButton(
-          onPressed: currentIndex > 0
-              ? () => navigateToProblem(context, previousProblemId, isNext: false)
-              : null,
-          style: _buildButtonStyle(themeProvider, screenHeight, isCompletion: false),
-          child: StandardText(
-            text: '< 이전 문제',
-            fontSize: screenHeight * 0.012,
-            color: themeProvider.primaryColor,
-          ),
-        ),
-        StandardText(
-          text: '${currentIndex + 1} / ${problemIds.length}',
-          fontSize: 16,
-          color: themeProvider.primaryColor,
-        ),
-        TextButton(
-          onPressed: nextProblemId != -1
-              ? () => navigateToProblem(context, nextProblemId, isNext: true)
-              : () => _showCompletionScreen(widget.practiceProvider),
-          style: _buildButtonStyle(themeProvider, screenHeight, isCompletion: nextProblemId == -1),
-          child: StandardText(
-            text: nextProblemId != -1 ? '다음 문제 >' : '복습 마치기',
-            fontSize: screenHeight * 0.012,
-            color: nextProblemId != -1 ? themeProvider.primaryColor : Colors.white,
-          ),
-        ),
+        buildPreviousButton(themeProvider, screenHeight),
+        buildProgressText(themeProvider),
+        buildNextOrCompleteButton(themeProvider, screenHeight),
       ],
     );
   }
 
-  void navigateToProblem(BuildContext context, int newProblemId,
-      {required bool isNext}) {
+  TextButton buildPreviousButton(ThemeHandler themeProvider, double screenHeight) {
+    final int currentIndex = getCurrentIndex();
+    final int previousProblemId = getPreviousProblemId(currentIndex);
+
+    return TextButton(
+      onPressed: currentIndex > 0
+          ? () => navigateToProblem(previousProblemId, isNext: false)
+          : null,
+      style: _buildButtonStyle(themeProvider, screenHeight, isCompletion: false),
+      child: StandardText(
+        text: '< 이전 문제',
+        fontSize: screenHeight * 0.012,
+        color: themeProvider.primaryColor,
+      ),
+    );
+  }
+
+  StandardText buildProgressText(ThemeHandler themeProvider) {
+    final int currentIndex = getCurrentIndex();
+    final int totalProblems = widget.practiceProvider.problemIds.length;
+
+    return StandardText(
+      text: '${currentIndex + 1} / $totalProblems',
+      fontSize: 16,
+      color: themeProvider.primaryColor,
+    );
+  }
+
+  TextButton buildNextOrCompleteButton(ThemeHandler themeProvider, double screenHeight) {
+    final int currentIndex = getCurrentIndex();
+    final int nextProblemId = getNextProblemId(currentIndex);
+
+    return TextButton(
+      onPressed: nextProblemId != -1
+          ? () => navigateToProblem(nextProblemId, isNext: true)
+          : () => _showCompletionScreen(),
+      style: _buildButtonStyle(themeProvider, screenHeight, isCompletion: nextProblemId == -1),
+      child: StandardText(
+        text: nextProblemId != -1 ? '다음 문제 >' : '복습 마치기',
+        fontSize: screenHeight * 0.012,
+        color: nextProblemId != -1 ? themeProvider.primaryColor : Colors.white,
+      ),
+    );
+  }
+
+  int getCurrentIndex() {
+    return widget.practiceProvider.problemIds.indexOf(widget.currentId);
+  }
+
+  int getPreviousProblemId(int currentIndex) {
+    final problemIds = widget.practiceProvider.problemIds;
+    return currentIndex > 0 ? problemIds[currentIndex - 1] : problemIds.first;
+  }
+
+  int getNextProblemId(int currentIndex) {
+    final problemIds = widget.practiceProvider.problemIds;
+    return currentIndex < problemIds.length - 1 ? problemIds[currentIndex + 1] : -1;
+  }
+
+  void navigateToProblem(int problemId, {required bool isNext}) {
     Navigator.pushReplacement(
       context,
       PageRouteBuilder(
         pageBuilder: (context, animation, secondaryAnimation) =>
             ProblemDetailScreenV2(
-              problemId: newProblemId,
+              problemId: problemId,
               isPractice: true,
             ),
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
@@ -108,11 +131,10 @@ class _PracticeNavigationButtonsState extends State<PracticeNavigationButtons> {
     );
   }
 
-  void _showCompletionScreen(ProblemPracticeProvider practiceProvider) {
-    // 현재 practiceId, totalProblems, practiceRound 정보를 전달합니다.
-    final practiceId = practiceProvider.currentPracticeId;
-    final totalProblems = practiceProvider.problemIds.length;
-    final practiceRound = practiceProvider.practiceThumbnails
+  void _showCompletionScreen() {
+    final practiceId = widget.practiceProvider.currentPracticeId;
+    final totalProblems = widget.practiceProvider.problemIds.length;
+    final practiceRound = widget.practiceProvider.practiceThumbnails
         ?.firstWhere((thumbnail) => thumbnail.practiceId == practiceId)
         .practiceCount ?? 0;
 
