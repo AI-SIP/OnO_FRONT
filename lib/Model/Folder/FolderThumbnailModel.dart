67aaec1f454b7a960614133a732f342c6124e3f6
KiSeungMin <ksm011212@gmail.com>
2026-06-30 13:15:06 +0900

    [Feat] 폴더 화면 사용성 개선 - 공책 문제 수 표시 드래그 삭제 추가

--- lib/Model/Folder/FolderThumbnailModel.dart
+++ lib/Model/Folder/FolderThumbnailModel.dart
@@ -2,21 +2,32 @@
 class FolderThumbnailModel {
   final int folderId;
   final String folderName;
+  final int problemCount;
 
   FolderThumbnailModel({
     required this.folderId,
     required this.folderName,
+    this.problemCount = 0,
   });
 
   factory FolderThumbnailModel.fromJson(Map<String, dynamic> json) {
     return FolderThumbnailModel(
       folderId: json['folderId'] as int,
       folderName: json['folderName'] as String,
+      problemCount: _parseProblemCount(json['problemCount']),
     );
+  }
+
+  static int _parseProblemCount(dynamic value) {
+    if (value == null) return 0;
+    if (value is int) return value;
+    if (value is num) return value.toInt();
+    return int.tryParse(value.toString()) ?? 0;
   }
 
   Map<String, dynamic> toJson() => {
         'folderId': folderId,
         'folderName': folderName,
+        'problemCount': problemCount,
       };
 }
