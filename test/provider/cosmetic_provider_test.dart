28c02500704292bbe738ffb5d466223689f0f7e8
KiSeungMin <ksm011212@gmail.com>
2026-09-11 21:44:45 +0900

    [Chore] 치장 에셋을 2026-09-11 번들로 교체 - SVG 열다섯 종이 좌표를 유지한 채 PNG 로 들어와 앱에서 다시 변환할 일이 없어졌고 겨울 귀마개 코랄 목도리 랜턴 공책 텀블러 다섯 종이 늘어 마흔 종이 된다 새로 온 것은 레벨 배치를 건드리지 않도록 미션 보상으로 넣는다

--- test/provider/cosmetic_provider_test.dart
+++ test/provider/cosmetic_provider_test.dart
@@ -215,7 +215,7 @@ void main() {
       final items = CosmeticMockData.loadout.items;
 
       expect(items.where((item) => item.unlocksByLevel), hasLength(16));
-      expect(items.where((item) => !item.unlocksByLevel), hasLength(19));
+      expect(items.where((item) => !item.unlocksByLevel), hasLength(24));
     });
   });
 }
